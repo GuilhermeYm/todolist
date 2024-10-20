f481de0304e45d93594e6ce7297dce01051dce32
Guilherme <88838651+GuilhermeYm@users.noreply.github.com>
2024-10-20 11:23:36 -0300

    Update next.config.mjs

--- todolistbico/next.config.mjs
+++ todolistbico/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/To-do-list-Bico',
+  assetPrefix: '/To-do-list-Bico/',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
